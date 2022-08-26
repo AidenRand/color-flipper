e494cd15efec37b69afc897f5890733d2cea0cd9
Cortrax <aidenrphotos@gmail.com>
2022-08-26 09:47:09 -0400

    fix page size

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ let text = document.querySelector(".text");
 button.addEventListener("click", () => {
   let colorNum = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.background = "#" + colorNum;
-  let color = text.textContent = "#" + colorNum;
+  let color = (text.textContent = "#" + colorNum);
+  return color;
 });
