d5b24f9824bf04ed7171d20552c558257ad0a8d6
Cortrax <aidenrphotos@gmail.com>
2022-08-26 09:44:59 -0400

    clean JavaScript

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 let button = document.querySelector(".btn");
 let text = document.querySelector(".text");
 
-function randomColor() {
-  let color = Math.floor(Math.random() * 16777215).toString(16);
-  return color;
-}
-randomColor();
-
 button.addEventListener("click", () => {
   let colorNum = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.background = "#" + colorNum;
